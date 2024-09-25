c7601aa1e666a6b2a10552c782a6b65d0060a30b
BOT22345 <nmongia48@gmail.com>
2024-09-25 20:43:03 +0530

    added the basic react frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/navbar/Navbar"
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+import {assets} from "../../assets/assets.js"
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+        <img src={assets.img} alt=""></img>
+    </div>
+  )
+}
