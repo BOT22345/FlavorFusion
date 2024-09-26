c495fa686ec1abe27a2e35bdce651fe664749423
BOT22345 <nmongia48@gmail.com>
2024-09-26 22:48:27 +0530

    refined the basic structure of navbar

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,13 @@
 import food from "./food.jpg"
 import logo from "./logo.jpeg"
+import searchIcon from "./searchIcon.png"
+import basketIcon from "./basketIcon.png"
+import logoText from "./logoText.jpg"
 
 export const assets={ 
     food:food, 
-    logo:logo}
+    logo:logo,
+    searchIcon:searchIcon,
+    basketIcon:basketIcon,
+    logoText:logoText
+}
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,13 +4,25 @@ import {assets} from "../../assets/assets.js"
 export const Navbar = () => {
   return (
     <div className="navbar">
-        <img className="logo" src={assets.logo}></img>
+        <div className="logoContainer">
+          <img className="logo" src={assets.logo}></img>
+          <img src={assets.logoText} className="logo-text"></img>
+        </div>
         <ul className="navbar-menu">
           <li>Home</li>
           <li>Menu</li>
           <li>Mobile-App</li>
           <li>Contact Us</li>
         </ul>
+        <div className="navbar-right">
+          <img src={assets.searchIcon} className="assets-search-icon"></img>
+          <div className="navbar-search-icon">
+            <img src={assets.basketIcon}className="basketIcon"></img>
+            <div className="dot"></div>
+          </div>
+          <button>sign in </button>
+        </div>
     </div>
+
   )
 }
