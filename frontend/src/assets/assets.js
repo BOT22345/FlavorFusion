21db7b30105d016c180ba330eaf42cc65a0d6281
BOT22345 <nmongia48@gmail.com>
2024-09-26 18:52:14 +0530

    added the logo file and basic navbar structure

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,6 @@
+import food from "./food.jpg"
+import logo from "./logo.jpeg"
+
+export const assets={ 
+    food:food, 
+    logo:logo}
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,7 +4,13 @@ import {assets} from "../../assets/assets.js"
 export const Navbar = () => {
   return (
     <div className="navbar">
-        <img src={assets.img} alt=""></img>
+        <img className="logo" src={assets.logo}></img>
+        <ul className="navbar-menu">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Mobile-App</li>
+          <li>Contact Us</li>
+        </ul>
     </div>
   )
 }
