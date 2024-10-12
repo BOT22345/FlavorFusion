3af76b9aaf1785a5ef748cad40b39e7cd0ec0c24
BOT22345 <nmongia48@gmail.com>
2024-10-12 12:27:42 +0530

    added the state and function to add and remove item in storecontext

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,9 +1,23 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext=createContext(null)
+export const StoreContext=createContext(null);
 
 const StoreContextProvider=(props)=>{
+
+    const [cartItem,setCartItem]=useState({});
+
+    const addProduct=(itemId)=>{
+        if(!cartItem[itemId]){
+            setCartItem((prev)=>({...prev,[itemId]:1}));
+        }else{
+            setCartItem((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        }
+    }
+
+    const removeProduct=(itemId)=>{
+        setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
 
     const contextValue={
         food_list
