c7dcf3a6c6eff69397c663c7e5b6ec1e3380e9c1
BOT22345 <nmongia48@gmail.com>
2024-10-14 10:05:43 +0530

    filtered the food products on the basis of category

--- frontend/src/components/fooddisplay/FoodDisplay.jsx
+++ frontend/src/components/fooddisplay/FoodDisplay.jsx
@@ -11,7 +11,9 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
+              if(category==="All" || category===item.category){
                 return (<FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}></FoodItem>)
+              }
             })}
         </div> 
     </div>
