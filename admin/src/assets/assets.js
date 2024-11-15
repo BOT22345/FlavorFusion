501143f45df128cf9b6e64732fd6cec83e94f54b
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2024-11-15 10:10:47 +0530

    added the basic setup for admin panel

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,11 @@
+import basketIcon from "./basketIcon.png";
+import coloredPlus from "./coloredPlus.jpg";
+import coloredSub from "./coloredSub/jpg";
+import logoText from "./logoText.jpg";
+
+export const assets={
+    basketIcon,
+    coloredPlus,
+    coloredSub,
+    logoText
+}
