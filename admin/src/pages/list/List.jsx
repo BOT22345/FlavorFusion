cc147be265629999a3696aa7428f23efd6bc4873
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2024-11-16 14:58:42 +0530

    added the structure for list component admin page

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
 import "./List.css";
+import axios from "axios";
 const List = () => {
+  const url="http://localhost:4000";
+  const [list,setList]=useState([]);
+  const fetchList=async ()=>{
+    const response=await axios.get(`${url}/api/food/list`);
+    console.log(response);
+    if(response.data.success){
+      setList(response.data.data);
+      alert("list loaded");
+    }
+    else{
+      alert("list fetch error");
+    }
+  }
+  useEffect(()=>{fetchList()},[]);
   return (
-    <div>List</div>
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index)=>{
+          return(
+            <div key={index} className="list-table-format">
+                <img src={`${url}/images/`+item.image}></img>
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>{item.price}</p>
+                <p>X</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
