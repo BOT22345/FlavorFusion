e090ea36aa3336fb92e3dca1940abdb84ea98e65
BOT22345 <153290500+Nayan2234@users.noreply.github.com>
2024-11-17 11:08:51 +0530

    added the logic for sign up and login in frontend

--- frontend/src/components/loginPopup/LoginPopup.jsx
+++ frontend/src/components/loginPopup/LoginPopup.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./LoginPopup.css";
 import {assets} from "../../assets/assets.js";
 const LoginPopup = ({setShowLogin}) => {
   const [currentState,setCurrentState]=useState("Sign-Up");
+  const [data,setData]=useState({
+    name:"",
+    email:"",
+    password:""
+  });
+
+  const onChangeHandler=(event)=>{
+    const name=event.target.name;
+    const value=event.target.value;
+    setData(data=>({...data,[name]:value}));
+  }
+
+  useEffect(()=>{
+    console.log(data);
+  },[data])
   return (
     <div className="login-popup">
         <form className="login-popup-container">
@@ -12,12 +27,12 @@ const LoginPopup = ({setShowLogin}) => {
             <img className="cross-icon" onClick={()=>{setShowLogin(false)}} src={assets.crossImage_icon}></img>
           <div className="login-popup-inputs">
             {currentState==="Sign-Up"?
-              <input type="text" placeholder="Your name" required></input>
+              <input type="text" name="name" onChange={onChangeHandler} value={data.name} placeholder="Your name" required></input>
               :
               <></>
             }
-            <input type="email" placeholder="Your email" required></input>
-            <input type="password" placeholder="Your password" required></input>
+            <input type="email" name="email" onChange={onChangeHandler} value={data.email} placeholder="Your email" required></input>
+            <input type="password" name="password" onChange={onChangeHandler} value={data.password} placeholder="Your password" required></input>
           </div>
           <button className="login-button">{currentState==="Sign-Up"?"Sign-Up":"Login"}</button>
           {
